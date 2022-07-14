eea4167c0d298625dfee4e095a13fb60185909ef
lvieira <contato@lucianovieirapro.com>
2022-07-14 10:11:17 -0300

    First commit!

---
+++ functions.py
@@ -0,0 +1,36 @@
+"""
+cores = ['amarelo', 'verde', 'vermelho', 'azul']
+print(cores)
+
+curso = 'Programação em Python: Essencial'
+
+print(curso)
+
+cores.append('azul')
+
+print(cores)
+
+cores.clear()
+print(cores)
+
+"""
+# Definindo a primeira função
+# Definição da função
+def diz_oi():
+	print('Oi!')
+
+"""
+Observação:
+1. Veja que , dentro das nossas funções podemos utilizar outras funções.
+2. Veja que nossa função só executa 1 tarefa, ou seja, a única coisa que faz é dizer oi:
+3. Veja que esta função não recebe nenhum parametro de entrada.
+3. Veja que esta função não retorna nada.
+"""
+
+# Chamada de execução
+diz_oi()
+"""
+Atenção:
+Nunca esqueça de utilizar o parênteses antes de executar uma função.
+
+"""
